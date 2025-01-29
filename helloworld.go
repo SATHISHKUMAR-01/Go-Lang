c7a5de51dd8e086d4b69f2132a2b9392f9b6305f
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2025-01-29 22:32:17 +0530

    Learnt abt constants

--- helloworld.go
+++ helloworld.go
@@ -7,11 +7,19 @@ import (
 func main() {
 	fmt.Println("Hello World!")
 
-	var num1 int = 1
+	var num1 int = 1 // var <variable_name> datatype
 	num2 := 2
 	var num3 int = 3
 
 	fmt.Println(num1)
 	fmt.Println(num2)
 	fmt.Println(num3)
+
+	const PI = 3.14
+	const rupees int = 1
+	fmt.Println(PI)
+	fmt.Println(rupees)
+
+	// Array Declaration
+
 }
