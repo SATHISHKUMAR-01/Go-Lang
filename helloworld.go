b22db8449471a48c7044c371f37d047387af6ecb
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2025-01-31 22:22:49 +0530

    Learnt abt slice

--- helloworld.go
+++ helloworld.go
@@ -49,4 +49,37 @@ func main() {
 	arr_mod := [...]int{1, 3: 4}
 	arr_len := len(arr_mod)
 	fmt.Println(arr_mod, arr_len)
+
+	// Slices
+
+	slice_1 := []int{}
+	fmt.Println(len(slice_1))
+	fmt.Println(cap(slice_1))
+
+	// Create a Slice From an Array
+
+	arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	slice_from_arr := arr[3:6]
+	fmt.Println((slice_from_arr))
+
+	// Create Slice from make
+
+	slice_make := make([]int, 5, 10)
+
+	fmt.Println("Slice len : ", len(slice_make))
+	fmt.Println("Slice cap : ", cap(slice_make))
+
+	// Append elements to slide
+
+	slice_append := []int{1, 2, 3}
+	slice_append = append(slice_append, 4, 5)
+	fmt.Println(slice_append)
+
+	myslice1 := []int{1, 2, 3}
+	myslice2 := []int{4, 5, 6}
+	myslice3 := append(myslice1, myslice2...)
+
+	fmt.Printf("myslice3=%v\n", myslice3)
+	fmt.Printf("length=%d\n", len(myslice3))
+	fmt.Printf("capacity=%d\n", cap(myslice3))
 }
