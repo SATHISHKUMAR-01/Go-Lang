24a0f95381d8a14339cb988ae431003fc45226f5
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2025-02-02 15:25:01 +0530

    Learn abt variadic, anonymous functions and map

--- helloworld.go
+++ helloworld.go
@@ -172,4 +172,83 @@ func main() {
 	student.age = 21
 
 	fmt.Println("Name : ", student.name, " Age : ", student.age)
+
+	// Create Map using var and :=
+
+	var a = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
+	b := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
+
+	fmt.Printf("a\t%v\n", a)
+	fmt.Printf("b\t%v\n", b)
+
+	fmt.Println(a["brand"])
+
+	// Create Maps Using make() Function
+
+	var map_a = make(map[string]string)
+	map_a["brand"] = "Ford"
+	map_a["model"] = "Mustang"
+	map_a["year"] = "1964"
+
+	map_b := make(map[string]int)
+	map_b["Oslo"] = 1
+	map_b["Bergen"] = 2
+	map_b["Trondheim"] = 3
+	map_b["Stavanger"] = 4
+
+	fmt.Printf("a\t%v\n", map_a)
+	fmt.Printf("b\t%v\n", map_b)
+
+	// Delete a key in map
+
+	delete(map_b, "Oslo")
+	fmt.Printf("b\t%v\n", map_b)
+
+	// Check For Specific Elements in a Map
+
+	// val, ok :=map_name[key]
+
+	var check_a = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964", "day": "Satur"}
+
+	val1, ok1 := check_a["brand"] // Checking for existing key and its value
+	val2, ok2 := check_a["color"] // Checking for non-existing key and its value
+	val3, ok3 := check_a["day"]   // Checking for existing key and its value
+	_, ok4 := check_a["model"]    // Only checking for existing key and not its value
+
+	fmt.Println(val1, ok1)
+	fmt.Println(val2, ok2)
+	fmt.Println(val3, ok3)
+	fmt.Println(ok4)
+
+	// Reference Map
+
+	ref_map := check_a
+	fmt.Println(ref_map)
+
+	// Iterate Over Maps
+
+	for k, v := range ref_map {
+		fmt.Println(k, v)
+	}
+
+	// Variadic function
+	fmt.Println("Sum of 1, 2, 3:", sumVariadic(1, 2, 3))
+
+	// Anonymous function in Go Language
+
+	value := func(data string) {
+		fmt.Println(data)
+	}
+	value("Anonymous functions")
+
+	// Defer Keyword in Golang
+
+}
+
+func sumVariadic(nums ...int) int {
+	total := 0
+	for n := range nums {
+		total += n
+	}
+	return total
 }
